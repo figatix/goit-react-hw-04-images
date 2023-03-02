2fb5b2aa97ec97d78933c9969a46db2a8608efe3
figatix <60873339+figatix@users.noreply.github.com>
2023-03-02 04:06:02 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,36 +1,40 @@
 import PropTypes from 'prop-types';
-import React, { useEffect, useCallback } from 'react';
+import React
+  // , { useEffect }
+  from 'react';
 import { createPortal } from 'react-dom';
 import { StyledModal, StyledOverlay } from './Modal.styled';
 
-const modalRoot = document.querySelector('#modal-root');
+const modalRoot = document.querySelector('#modal-root')
 
-const Modal = ({ onCloseModal, children }) => {
-  const handlerCloseModal = useCallback((e) => {
-    const isEscBtn = e.code === 'Escape';
+export const Modal = ({ onCloseModal, children }) => {
+
+  // useEffect(() => {
+  //   window.addEventListener('keydown', handlerCloseModal)
+  //   return () => window.removeEventListener('keydown', handlerCloseModal);
+  // }, [])
+
+  const handlerCloseModal = (e) => {
+    const isEscBtn = e.code === "Escape";
     const isBackdrop = e.target === e.currentTarget;
 
     if (isEscBtn || isBackdrop) {
-      onCloseModal();
+      onCloseModal()
     }
-  }, [onCloseModal]);
-
-  useEffect(() => {
-    window.addEventListener('keydown', handlerCloseModal);
-
-    return () => {
-      window.removeEventListener('keydown', handlerCloseModal);
-    };
-  }, [handlerCloseModal]);
+  }
 
   return createPortal(
     <StyledOverlay onClick={handlerCloseModal}>
-      <StyledModal>{children}</StyledModal>
+      <StyledModal >
+        {children}
+      </StyledModal>
     </StyledOverlay>,
-    modalRoot,
-  );
-};
+    modalRoot
+  )
+}
+
+
 
 Modal.propTypes = {
   onCloseModal: PropTypes.func.isRequired,
-};
+}
